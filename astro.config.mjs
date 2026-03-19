9d2feff2bfcf2a10363d270adfb36fb9956deb04
Keishmer Pérez <145237779+Keishmer00@users.noreply.github.com>
2026-03-19 16:41:19 -0500

    act

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
     site: 'https://github.com/Design-Wise-Gold/Landing_WiseGold',   
-    base: '/Landing_WiseGold',
+    base: '/',
 	vite: {
 		plugins: [tailwindcss()],
 	},
