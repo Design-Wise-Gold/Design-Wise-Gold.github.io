8ad8c3a16dda2efb12653f84582f75f4090bc934
Keishmer Pérez <145237779+Keishmer00@users.noreply.github.com>
2026-04-22 17:02:03 -0500

    actualizaciones

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://github.com/Design-Wise-Gold/Landing_WiseGold',   
-    base: '/',
+    site: 'https://wise.gold',
 	vite: {
 		plugins: [tailwindcss()],
 	},
+	integrations: [sitemap()],
 });
